39c11fd1161c82f4ac0f74c29732c8749b62220c
anton feldmann <anton.feldmann@gmail.com>
2017-10-04 14:27:19 +0900

    clean cli

--- asimov.go
+++ asimov.go
@@ -11,32 +11,4 @@ func main() {
      if err := cmd.Asimov.Execute(); err != nil {
      	log.Fatal(err)
      }
-
-	/*
-
-				{
-					Name:  "ascii",
-					Usage: "*.ls, *.va, *.dat, *.dg, *.xml",
-					Action: func(c *cli.Context) {
-						bak.Backup(func(filename string) bool {
-							switch filepath.Ext(filename) {
-							case ".ls", ".va", ".dat", ".dg", ".xml":
-								return true
-							}
-							return false
-						}, "ascii")
-					},
-				},
-			},
-		},
- 		{
- 			Name:      "remove",
- 			ShortName: "r",
- 			Usage:     "remove a robot",
- 			Action: func(c *cli.Context) {
-				bak.removeRobot()
- 			},
- 		},
-	}
-	app.Run(os.Args)*/
 }
